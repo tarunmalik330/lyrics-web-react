983e7ef64b707c55a039b5d2d23b7c2b8441c08b
tarunmalik330 <maliktarun2005@gmail.com>
2025-01-23 11:20:05 +0530

    tailwind install

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-white font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
